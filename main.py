6a4becd1f9be56d5c913f1c252f1fcc589b4f94f
NikooSabzevar <nikoo.sabzevar@gmail.com>
2024-01-14 21:26:17 -0600

    Modify and add terminate

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import uuid
 import os
+import time
+import keyboard
+import sys
+import psutil
 
 
 if "rows" not in st.session_state:
@@ -30,23 +34,15 @@ def generate_row(row_id):
     row_columns[4].button("🗑️", key=f"del_{row_id}", on_click=remove_row, args=[row_id])
     return {"dates": dates, "game": row_name, "buyin": row_qty1, 'cashout': row_qty2}
 
-st.title("CASINO")
-dates_played = []
+st.title("CASINO TRACKER")
 
 for row in st.session_state["rows"]:
     
     row_data = generate_row(row)
     rows_collection.append(row_data)
-    #dates_played.append(str(dates))
 
 entries = pd.DataFrame(rows_collection)
 entries.dropna(inplace = True)
-#st.write(pd.DataFrame(rows_collection))
-
-
-menu = st.columns(2)
-with menu[0]:
-    st.button("Add Games/Entries", on_click=add_row)
 
 
 if os.path.isfile("Casino.csv"): 
@@ -59,6 +55,7 @@ else:
     casino_df = pd.DataFrame()
 
 final_table = pd.concat([entries, casino_df], ignore_index= True)
+
 #st.write(final_table)
 
 balances = {'Poker': 0, "Crabs": 0, "Blackjack": 0, "Roulette": 0, "Slots": 0}
@@ -73,66 +70,71 @@ try:
 except: 
     pass
 
-Balances = {"Games": balances.keys(), "Balance ($)": balances.values()}
+Balances = pd.DataFrame.from_dict({"Games": balances.keys(), "Balance ($)": balances.values()})
+final_table['balance'] = final_table['cashout'] - final_table['buyin']
 
-total_balance = sum(balances.values())
+
+menu = st.columns(2)
+with menu[0]:
+    st.button("Add Games/Entries", on_click=add_row)
+    #Balances.to_csv('temp.csv')
+    #final_table.to_csv("Casino.csv")
+
+
+
+#@st.cache
+total_balance = final_table['balance'].sum()
 T_balance = 'Total Balance $' + str(total_balance)
-if total_balance >= 0: 
+if total_balance > 0: 
     new_title = f'<p style="font-family:sans-serif; color:Green; font-size: 32px;">{T_balance}</p>'
-else: 
+elif total_balance < 0: 
     new_title = f'<p style="font-family:sans-serif; color:Red; font-size: 32px;">{T_balance}</p>'
+else:
+    new_title = f'<p style="font-family:sans-serif; color:Black; font-size: 32px;">{T_balance}</p>'
 
 st.markdown(new_title, unsafe_allow_html=True)
 #st.title(new_title)
 df_games = pd.DataFrame(Balances).set_index('Games').rename_axis('Games')
-#df_games.drop(index = 'Games', inplace = True)
-df_2 = df_games.style.applymap(lambda x: 'color: red' if int(x) < 0 else 'color: green')
-#st.dataframe(df_2)
+df_games['Balance ($)'].apply(lambda x: round(x, 2))
 
-#st.write(df_2.to_html(), unsafe_allow_html= True)
-############ Styles
-th_props = [('font-size', '12px'), ('text-align', 'center'), ('font-weight','bold'), ('color', '#6d6d6d')]
-td_props = [('font-size', '12px')]
-styles = [dict(selector = 'th', props = th_props), dict(selector = 'td', props = td_props)]
-###########
-#df_2 = df_games.set_table_style(styles)
-#df_games.rename_axis("Games")
+
+#df_games.drop(index = 'Games', inplace = True)
+def color_condition(x): 
+    if x < 0:
+        return 'color : red'
+    elif x > 0: 
+        return 'color : green'
+    else: 
+        return 'color: black' 
+    
+df_2 = df_games.style.applymap(color_condition).format({"Balance ($)": "{:.1f}".format})
 st.table(df_2)
 
-with menu[1]:
+###### Download as local #######
+from datetime import date 
 
-    if st.button("End & Submit Entries"):
+#temp_local = pd.read_csv('Casino.csv')
 
-        final_table.to_csv("Casino.csv")
+final_table['dates'] = pd.to_datetime(final_table['dates'])
+final_table.sort_values(["dates"],  axis=0, ascending=[False], ignore_index=True, inplace=True)
+try: 
+    final_table.drop(['level_0', 'index'], axis = 1, inplace = True)
+except: 
+    pass 
 
-
-#df_balance = pd.DataFrame(rows_collection)
-
-
-    # if os.path.isfile("Casino.csv"): 
-    #     casino_df = pd.read_csv('Casino.csv')
-    #     try: 
-    #         casino_df.drop('Unnamed: 0', axis = 1, inplace = True)
-    #     except: 
-    #         pass
-
-    #     df_balance = pd.concat([df_balance, casino_df], ignore_index=True)
-    # else: 
-    #     pass 
-# df_balance.to_csv("Casino.csv", index = False)
-
-#st.write(data)
-#st.write(casino_df)
-
-#if len(rows_collection) > 0:
-    #st.subheader("Collected Data")
-    #display = st.columns(3)
-    #data = pd.DataFrame(rows_collection)
-    #data.to_csv("Casino.csv", mode='a', index= False, header = False)
-    #st.write(data)
-    #data.rename(columns={"name": "Item Name", "Buyin": "Buyin", "Cashout": "Cashout"}, inplace=True)
-    #display[0].dataframe(data=data, use_container_width=True)
-    #display[1].bar_chart(data=data, x="Item Name", y="Quantity")
+#final_table.reset_index(drop = True) 
+csv = final_table.to_csv()
+st.download_button(label = "Download the master sheet", data = csv, file_name = str(date.today())+'.csv')
 
 
-
+exit_app = st.sidebar.button("Submit & Close")
+if exit_app:
+    final_table.to_csv("Casino.csv") 
+    # Give a bit of delay for user experience
+    time.sleep(2)
+    # Close streamlit browser tab
+    keyboard.press_and_release('ctrl+w')
+    # Terminate streamlit python process
+    pid = os.getpid()
+    p = psutil.Process(pid)
+    p.terminate()
